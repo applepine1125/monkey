e5868837f2fbca11d7ff681860810cba71fcfc06
applepine1125 <itn.siknh.bonvoyage@gmail.com>
2017-08-29 16:46:54 +0900

    :rocket: Done 2.8 Function Literals

--- ast/ast.go
+++ ast/ast.go
@@ -2,6 +2,7 @@ package ast
 
 import (
 	"bytes"
+	"strings"
 
 	"github.com/applepine1125/monkey/token"
 )
@@ -79,6 +80,12 @@ type IfExpression struct {
 type BlockStatement struct {
 	Token      token.Token
 	Statements []Statement
+}
+
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
 }
 
 func (rs *ReturnStatement) statementNode()       {}
@@ -209,4 +216,24 @@ func (bs *BlockStatement) String() string {
 	}
 
 	return out.String()
+}
+
+func (fl *FunctionLiteral) expressionNode()      {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(")")
+	out.WriteString(fl.Body.String())
+
+	return out.String()
+
 }
